84239f87bd5744f3843bfa159338fd072350490d
massimopallera <pallera.massimo@gmail.com>
2025-04-07 17:34:04 +0200

    initial commit

---
+++ src/main/java/org/lessons/java/gamebase/GamebaseApplication.java
@@ -0,0 +1,13 @@
+package org.lessons.java.gamebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamebaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lessons/java/gamebase/GamebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.java.gamebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
