2f17b2a15b8af1245e896034abf506d9a05f2034
massimopallera <pallera.massimo@gmail.com>
2025-04-08 20:26:00 +0200

    fix bugs

--- src/main/java/org/lessons/java/gamebase/Model/User.java
+++ src/main/java/org/lessons/java/gamebase/Model/User.java
@@ -3,6 +3,7 @@ package org.lessons.java.gamebase.Model;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,7 +28,7 @@ public class User {
     @NotBlank
     private String password;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
         name = "role_user",
         joinColumns = @JoinColumn(name = "user_id"),
